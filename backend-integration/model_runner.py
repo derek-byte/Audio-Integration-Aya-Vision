dfe9681a8af6d2d84650f938273eace13d3ee699
Abhay Joshi <jabhay2012@gmail.com>
2025-04-17 22:03:58 -0400

    Renamed stt-audio to stt_audio for Python import compatibility

---
+++ backend-integration/model_runner.py
@@ -0,0 +1,8 @@
+def transcribe(audio_path, model="whisper", model_size="base"):
+    """ 
+    audio_path: The path to the audio file that needs to be transcribed
+    model: The model that we will be using to transcribe the audio file, defaults to whisper
+    size: The varient of the model that we will be using. defaults to 'whisper-base'
+    """
+   
+    pass
