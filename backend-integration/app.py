d59f19cac335da7e3c28ce13e60b66a2c48807f1
Olufemi <femiosinkolu@gmail.com>
2025-05-06 03:20:24 +0100

    feat!: add TTS model

--- backend-integration/app.py
+++ backend-integration/app.py
@@ -3,9 +3,9 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
-from flask import Flask, jsonify, request 
-from utils import save_audio_file
-from model_runner import transcribe
+from flask import Flask, jsonify, request, send_file
+from utils import save_audio_file, save_waveform
+from model_runner import transcribe,load_model_and_tokenizer, synthesize_speech
 
 app = Flask(__name__)
 
@@ -35,6 +35,19 @@ def transcribe_audio():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
         
+@app.route("/synthesize", methods=["POST"])
+def synthesize():
+    tokenizer, model = load_model_and_tokenizer()
+    data = request.json
+    text = data.get("text", "").strip()
+
+    if not text:
+        return jsonify({"error": "Text input is required"}), 400
+    waveform = synthesize_speech(text, tokenizer, model)
+    audio_path = save_waveform(waveform)
+    return send_file(audio_path, mimetype="audio/wav", as_attachment=True, download_name="output.wav")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
